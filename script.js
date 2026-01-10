735a2cc9a774484d1a7a29aa573926c2d24710e3
vishalexplore <vishalsharma95980@gmail.com>
2026-01-10 13:43:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "f0377fdf28a6b842a29fcae83354d412"
+const API_KEY = "a935871e11dc6bf3ca8a5d48463cf6dc"
 const BASE_URL = "https://api.openweathermap.org/data/2.5"
 const GEO_URL = "https://api.openweathermap.org/geo/1.0"
 
